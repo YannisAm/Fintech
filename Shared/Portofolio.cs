fe179e6d64f6049b30b06a45624fcc01156447bd
Yannis <giannamanat@gmail.com>
2022-09-14 22:48:34 +0300

    Initialize Portfolio and Security modelsAdd project files.

---
+++ Shared/Portofolio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fintech.Shared
+{
+    public class Portofolio
+    {
+        public int Id { get; set; }
+        public string NameOfSecurity { get; set; } = string.Empty;
+        public float Price { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public DateTime DateTimeObtained { get; set; }
+    }
+}
---
+++ Shared/Securities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fintech.Shared
+{
+    public class Securities
+    {
+        public int Id { get; set; }
+        public string SecurityName { get; set; } = string.Empty;
+        public float Price { get; set; }
+        public DateTime DateTimeObtained { get; set; } = DateTime.Now; //  apply an aoption for user to change it if he wants
+
+
+    }
+}
