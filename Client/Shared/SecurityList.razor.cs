e97848b2378ca8a72ddc163ee57591ba9e70ac98
Giannis Amanatidis <giannamanat@outlook.com>
2022-09-20 13:18:13 +0300

    Finished styling portfolio table. Need to fix the sum of stocks and the value of stocks

--- Client/Shared/SecurityList.razor.cs
+++ Client/Shared/SecurityList.razor.cs
@@ -1,17 +1,20 @@
 ﻿using Fintech.Shared.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fintech.Client.Shared
 {
     public partial class SecurityList : ComponentBase
     {
-        private static List<Security> Securities = new List<Security>
+        internal static List<Security> Securities = new List<Security>
         {
             new Security
             {
                 Id = 1,
                 SecurityName = "VUAA",
                 Price = 152.69f,
+                StockesOwned = 3,
+                StocksValue = 458.07f,
                 DateTimeObtained = DateTime.Now,
                 Description = "The best ETF"
             },
@@ -20,6 +23,8 @@ namespace Fintech.Client.Shared
                 Id = 2,
                 SecurityName = "MSFT",
                 Price = 352.42f,
+                StockesOwned = 10,
+                StocksValue = 3524.2f,
                 DateTimeObtained = DateTime.Now,
                 Description = "The best MSFT"
             },
@@ -28,9 +33,29 @@ namespace Fintech.Client.Shared
                 Id = 3,
                 SecurityName = "APPL",
                 Price = 1253.88f,
+                StockesOwned = 100,
+                StocksValue = 12538.8f,
                 DateTimeObtained = DateTime.Now,
                 Description ="The best technological company"
             }
         };
+
+        private static int StockesOwned (int sum)
+        {
+            foreach (var stokes in Securities)
+            {
+                sum += stokes.StockesOwned;
+            }
+            return sum;
+        }
+
+        private static float ValueOfStocks (float sumOfValue)
+        {
+            foreach (var value in Securities)
+            {
+                sumOfValue += value.StocksValue;
+            }
+            return sumOfValue;
+        }
     }
 }
