4cc3125f04abd38787e7b3f35a7f88be0a43cd97
Yannis <giannamanat@gmail.com>
2022-09-20 21:57:50 +0300

    Initialize DbContext

---
+++ Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Fintech.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fintech.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Security> Securities { get; set; }
+    }
+}
