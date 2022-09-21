26d481d0f224ebcbabbe2048662f9bd2789f8831
Yannis <giannamanat@gmail.com>
2022-09-21 20:24:29 +0300

    Finished Security migration to Db I leave the branch open for future changes

--- Server/Controllers/SecurityController.cs
+++ Server/Controllers/SecurityController.cs
@@ -1,4 +1,5 @@
-﻿using Fintech.Shared.Models;
+﻿using Fintech.Server.Data;
+using Fintech.Shared.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,44 +9,22 @@ namespace Fintech.Server.Controllers
     [ApiController]
     public class SecurityController : ControllerBase
     {
-        private static List<Security> Securities = new List<Security>
-        {
-            new Security
-            {
-                Id = 1,
-                SecurityName = "VUAA",
-                Price = 152.69f,
-                StockesOwned = 3,
-                StocksValue = 458.07f,
-                DateTimeObtained = DateTime.Now,
-                Description = "The best ETF"
-            },
-            new Security
-            {
-                Id = 2,
-                SecurityName = "MSFT",
-                Price = 352.42f,
-                StockesOwned = 10,
-                StocksValue = 3524.2f,
-                DateTimeObtained = DateTime.Now,
-                Description = "The best MSFT"
-            },
-            new Security
-            {
-                Id = 3,
-                SecurityName = "APPL",
-                Price = 1253.88f,
-                StockesOwned = 100,
-                StocksValue = 12538.8f,
-                DateTimeObtained = DateTime.Now,
-                Description ="The best technological company"
-            }
-        };
+        private readonly DataContext _context;
 
-        [HttpGet]
-        public async Task<ActionResult<List<Security>>> GetSecurity ()
+        public SecurityController(DataContext context)
         {
-            return Ok(Securities);
+            _context = context; 
+        }
+        
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<Security>>>> GetSecurity ()
+        {
+            var securities = await _context.Securities.ToListAsync();
+            var response = new ServiceResponse<List<Security>>()
+            {
+                Data = securities
+            };
+            return Ok(response);
         }
     }
 }
