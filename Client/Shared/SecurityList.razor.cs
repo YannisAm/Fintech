9c77faadea56f2b26369a06928b669843a32730d
Yannis <giannamanat@gmail.com>
2022-11-30 21:02:30 +0200

    -Fixed it

--- Client/Shared/SecurityList.razor.cs
+++ Client/Shared/SecurityList.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Fintech.Shared.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 using System.Runtime.CompilerServices;
 
 namespace Fintech.Client.Shared
@@ -14,8 +15,9 @@ namespace Fintech.Client.Shared
         public NavigationManager NavigationManager { get; set; }
         private List<Security> Securities = new();
         private Portfolio Portfolio= new Portfolio();
-        private List<string> names = new ();
-        private int cnt = 0;
+        List<string> names = new List<string>();
+        int iterator = 0;              //We use this integer as an itirator to run the whole name list and to present every element on frontend
+
 
         private static float ValueOfEachStock(Security security)
             => security.Price * security.StockesOwned;
@@ -41,21 +43,15 @@ namespace Fintech.Client.Shared
             return numberOfStocks;
         }
 
-        protected override async Task OnInitializedAsync()
-        {
+        protected override async Task OnInitializedAsync()                      //When the page is rendered we bring all the securities. Furthermore, we are getting the portfolios
+        {                                                                       //and assigning them in a list, because we want to use them in the razor page.
             Securities = await SecurityService.GetSecurities();
             foreach (var security in Securities)
-                names.Add(await PortfolioService.GetPortfolioNameBySecurity(security));
+            {
+                Portfolio = await PortfolioService.GetPortfolioById(security.PortfolioId);
+                names.Add(Portfolio.NameOfPortfolio);
+            }
         }
-
-        //protected override async Task OnParametersSetAsync()
-        //{
-        //    foreach (var security in Securities)
-        //    {
-        //        Portfolio = await PortfolioService.GetPortfolioById(security.PortfolioId);
-        //        security.Portfolio.NameOfPortfolio = Portfolio.NameOfPortfolio;
-        //    }
-        //}
 
         private string CutTheText(string description)
         {
