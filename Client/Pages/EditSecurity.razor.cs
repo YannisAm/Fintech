e6f6ce15a5f164a2e0237559602b82758248b45a
Yannis <giannamanat@gmail.com>
2022-12-04 12:15:59 +0200

    -On the way to solve it

--- Client/Pages/EditSecurity.razor.cs
+++ Client/Pages/EditSecurity.razor.cs
@@ -8,27 +8,37 @@ namespace Fintech.Client.Pages
         [Inject]
         public ISecurityService SecurityService { get; set; }
         [Inject]
+        public IPortfolioService PortfolioService { get; set; }
+        [Inject]
         public NavigationManager NavigationManager { get; set; }
 
-        private Security? security = null;
-
+        private Security security;
+        private List<Fintech.Shared.Models.Portfolio> Portfolios = new();
         [Parameter]
         public int Id { get; set; }
+        private Fintech.Shared.Models.Portfolio portfolaki;
+        private int portfolakiId;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Portfolios = await PortfolioService.GetPortfolios();
+        }
 
         protected override async Task OnParametersSetAsync()
         {
             security = await SecurityService.GetSecurityById(Id);
+
         }
 
-        public async Task Edit(Security security)
+        async Task HandleSubmit()
         {
             await SecurityService.EditSecurity(security);
-            Navigate();
+            //Navigate();
         }
 
         private void Navigate()
         {
-            NavigationManager.NavigateTo("/portfolio", true);
+            NavigationManager.NavigateTo("/securities", true);
         }
     }
 }
